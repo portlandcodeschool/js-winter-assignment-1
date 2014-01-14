8617da047e26190e12f43387abe2cbc121c4b134
Sean Sauve <seantsauve@gmail.com>
2014-01-13 20:50:10 -0800

    first assign1 commit

---
+++ seanSauve/monAssign1.js
@@ -0,0 +1,14 @@
+var myName = "Sean Sauve";
+var currentYear = 2014;
+var birthYear = 1986;
+var myAge = 28;
+var decades = Math.floor(myAge / 10);
+var yearRemainder = myAge % 10;
+var birthPlace = "Chicago";
+var mom = "Donna";
+var dad = "Jerry";
+console.log('Hi my name is ' + myName);
+console.log('I will be ' + myAge +' this year');
+console.log('I have been alive for', decades, 
+	'decades and', yearRemainder, 'years.');
+console.log('first line \nsecondline')
