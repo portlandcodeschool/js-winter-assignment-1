a4c3301ff8808c6734da99afa220f3af4b3319ef
Ian Forrest <ianforrest@gmail.com>
2014-01-14 19:06:44 -0800

    making changes

--- ianForrest/monAssign1.js
+++ ianForrest/monAssign1.js
@@ -8,12 +8,11 @@ var birthPlace = "Calgary, Canada";
 var mom = "Barbara";
 var dad = "Robert";
 
-console.log("You are invited to the wedding of Reese Witherspoon and " + myName + ".");
-console.log("Reese will be 37 this year. Ian will be " + myAge + ".");
-console.log("In other words, Ian will be " + decades + " and " + 
+console.log("Good morning, my name is " + myName + ".");
+console.log("This year, I will turn " + myAge + ".");
+console.log("In other words, I will be " + decades + " and " + 
 	yearRemainder);
-console.log("Here are some facts about the groom: \n \t Birthplace: " + birthPlace + "\n \t Mother's name: " + mom + "\n \t Father's name: " + dad); 
+console.log("Here are some fun facts about myself: \n \t Birthplace: " + birthPlace + "\n \t Mother's name: " + mom + "\n \t Father's name: " + dad); 
 
 if (myAge > 30) {console.log("Oh my, how the years have passed.")};
 
-// Struggling with github.
