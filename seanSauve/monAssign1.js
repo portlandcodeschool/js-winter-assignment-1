2995a67e1b11b9b7b4f7ac185cc19cb61d5d2d77
Sean Sauve <seantsauve@gmail.com>
2014-01-14 19:59:40 -0800

    Sean's first awesome homework

--- seanSauve/monAssign1.js
+++ seanSauve/monAssign1.js
@@ -7,8 +7,15 @@ var yearRemainder = myAge % 10;
 var birthPlace = "Chicago";
 var mom = "Donna";
 var dad = "Jerry";
-console.log('Hi my name is ' + myName);
-console.log('I will be ' + myAge +' this year');
+console.log('Hi my name is',  myName);
+console.log('I will be', myAge, 'years old this year');
 console.log('I have been alive for', decades, 
 	'decades and', yearRemainder, 'years.');
-console.log('first line \nsecondline')
+//notes look like this in java
+console.log('Here are my vital stats:', '\n     Birthplace:', 
+	birthPlace, '\n     Mother:', mom, '\n     Father:', dad);
+if (myAge > 30) {console.log('Oh my, how the years have passed.')};
+
+/*multi line comments
+look like this */
+
