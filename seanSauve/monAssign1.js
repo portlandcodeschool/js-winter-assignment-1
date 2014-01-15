dca385e8d9aefbcc3ee9bb0095dc360c892c751b
Sean Sauve <seantsauve@gmail.com>
2014-01-15 09:46:51 -0800

    monAssign1 actually correct version

--- seanSauve/monAssign1.js
+++ seanSauve/monAssign1.js
@@ -1,3 +1,8 @@
+//Sean Sauve
+//Portland Code School
+//Javascript course
+//Monday Assignment #1
+
 var myName = "Sean Sauve";
 var currentYear = 2014;
 var birthYear = 1986;
@@ -9,13 +14,11 @@ var mom = "Donna";
 var dad = "Jerry";
 console.log('Hi my name is',  myName);
 console.log('I will be', myAge, 'years old this year');
-console.log('I have been alive for', decades, 
-	'decades and', yearRemainder, 'years.');
-//notes look like this in java
-console.log('Here are my vital stats:', '\n     Birthplace:', 
-	birthPlace, '\n     Mother:', mom, '\n     Father:', dad);
-if (myAge > 30) {console.log('Oh my, how the years have passed.')};
+console.log('I have been alive for', decades, 'decades and', yearRemainder, 'years.');
+console.log('Here are my vital stats:', '\n\tBirthplace:',birthPlace, '\n\tMother:', mom, '\n\tFather:', dad);
+if (myAge > 30) {console.log('Oh my, how the years have passed.')};	// prints nastalgic comment if over 30 years old
 
+//comments look like this in java
 /*multi line comments
 look like this */
 
